458762d99858be347e5591d70ca5bfff918be881
Timur Khaimovich <itima.com@gmail.com>
2019-05-04 11:06:35 +0600

    Baza bratkov

---
+++ 14/SuperBaza/SuperBaza/Dosje.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBaza
+{
+    public class Dosje
+    {
+        DataSet ds;
+        SQLiteConnection con;
+        SQLiteCommand prikaz;
+        public Dosje()
+        {
+            string path = @"..\..\..\Bratki.db";
+            con = new SQLiteConnection("Data Source =" + path);
+        }
+
+        public void Vypolnyaj(string ukaz)
+        {
+            con.Open();
+            prikaz = new SQLiteCommand(ukaz, con);
+            prikaz.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataTable DaiInfu()
+        {
+            con.Open();
+            string ukaz = "select * from Bratki";
+            DataSet ds = new DataSet();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(ukaz, con);
+            adapter.Fill(ds);
+            con.Close();
+            return ds.Tables[0];
+        } 
+    }
+}
---
+++ 14/SuperBaza/SuperBaza/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperBaza
+{
+    public partial class Form1 : Form
+    {
+        Dosje infa = new Dosje();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Refresher();
+        }
+        private void Refresher()
+        {
+            infa = new Dosje();
+            TelikIzAeroporta.DataSource = infa.DaiInfu();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            infa.Vypolnyaj("insert into Bratki(Klikuha) values('" +textBox1.Text +"')");
+        }
+    }
+}
